084be515ca59756aa523f66c10c07c1191bb2e9b
Pieter van Mill <pietervanmill@hotmail.com>
2022-12-10 14:33:26 +0100

    Add the geo details of a place on a map.

---
+++ src/components/detail-map.js
@@ -0,0 +1,77 @@
+import { useEffect, useRef, useState } from "react";
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import '../style/map.css';
+
+function DetailMap(props) {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [API_KEY] = useState('dyK35oSh2RzcM1TQJdy8');
+
+    useEffect(() => {
+        if (map.current) return;
+
+        map.current = new maplibregl.Map({
+            container: mapContainer.current,
+            style: `https://api.maptiler.com/maps/ff2bdd4a-7b41-466d-9c1a-7331ec687f1b/style.json?key=${API_KEY}`,
+            center: [Number(props.longitude), Number(props.latitude)],
+            zoom: 12,
+            maxZoom: 14
+        });
+
+        map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
+    }, [API_KEY, props.latitude, props.longitude]);
+
+    useEffect(() => {
+        if (props.latitude && props.longitude) {
+            new maplibregl.Marker()
+                .setLngLat([Number(props.longitude), Number(props.latitude)])
+                .setPopup(new maplibregl.Popup({
+                    closeButton: false
+                }).setHTML(
+                    `<a class="text-gray-600 text-lg">${props.name}</a>`
+                ))
+                .addTo(map.current);
+        }
+    }, [map, props.latitude, props.longitude, props.name]);
+
+    useEffect(() => {
+        if (map.current && props.polygon) {
+            const data = JSON.parse(props.polygon);
+
+            map.current.on('load', () => {
+                map.current.addSource('polygon', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Polygon',
+                            'coordinates': [
+                                data
+                            ]
+                        }
+                    }
+                });
+
+                map.current.addLayer({
+                    'id': 'polygon',
+                    'type': 'fill',
+                    'source': 'polygon',
+                    'layout': {},
+                    'paint': {
+                        'fill-color': '#D3D3D3',
+                        'fill-opacity': 0.35
+                    }
+                });
+            });
+        }
+    }, [map, props.polygon]);
+
+    return (
+        <div className="map-inline">
+            <div ref={mapContainer} className="map" />
+        </div>
+    )
+}
+
+export default DetailMap;
--- src/pages/place-details.js
+++ src/pages/place-details.js
@@ -1,6 +1,7 @@
 import { Transition } from "@headlessui/react";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import DetailMap from "../components/detail-map";
 import PageLayout from "../components/page-layout";
 import Translation from "../components/translation";
 import TranslationNew from "../components/translation-new";
@@ -44,46 +45,62 @@ function PlaceDetails() {
                     {defaultTranslation.name}
                 </h2>
 
-                <div className="flex mt-4 items-center">
-                    <h2 className="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight">
-                        Translations
-                    </h2>
+                <section>
+                    <div className="flex mt-4 items-center">
+                        <h2 className="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight">
+                            Translations
+                        </h2>
 
-                    <button
-                        onClick={() => setAddingTranslation(!addingTranslation)}
-                        type="button"
-                        className="ml-auto inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        {addingTranslation && <span>Done</span>}
-                        {!addingTranslation && <span>Add translation</span>}
-                    </button>
-                </div>
+                        <button
+                            onClick={() => setAddingTranslation(!addingTranslation)}
+                            type="button"
+                            className="ml-auto inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            {addingTranslation && <span>Done</span>}
+                            {!addingTranslation && <span>Add translation</span>}
+                        </button>
+                    </div>
 
-                <div className="mt-4 overflow-hidden bg-white shadow sm:rounded-md">
-                    <ul className="divide-y divide-gray-200">
-                        <Transition
-                            show={addingTranslation}
-                            enter="transition-opacity duration-200"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100">
-                            <TranslationNew 
-                                placeholder="New York" 
-                                entityId={{place_id: id}}
-                                endpoint="/place/translation"
-                                setEntity={(t) => updateTranslations(t)}/>
-                        </Transition>
-
-                        {place.translations.map((tr) => (
-                            <li key={tr.id}>
-                                <Translation 
-                                    translation={tr}
-                                    endpoint="/place/translation"
+                    <div className="mt-4 overflow-hidden bg-white shadow sm:rounded-md">
+                        <ul className="divide-y divide-gray-200">
+                            <Transition
+                                show={addingTranslation}
+                                enter="transition-opacity duration-200"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100">
+                                <TranslationNew 
                                     placeholder="New York" 
-                                    defaultTranslation={tr.id === defaultTranslation.id} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                                    entityId={{place_id: id}}
+                                    endpoint="/place/translation"
+                                    setEntity={(t) => updateTranslations(t)}/>
+                            </Transition>
+
+                            {place.translations.map((tr) => (
+                                <li key={tr.id}>
+                                    <Translation 
+                                        translation={tr}
+                                        endpoint="/place/translation"
+                                        placeholder="New York" 
+                                        defaultTranslation={tr.id === defaultTranslation.id} />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </section>
+
+                <section className="mt-4">
+                    <h2 className="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight">
+                        Map
+                    </h2>
+                
+                    <div className="mt-4">
+                        <DetailMap 
+                            latitude={place.latitude}
+                            longitude={place.longitude}
+                            polygon={place.polygon}
+                            name={defaultTranslation.name}/>
+                    </div>
+                </section>
             </PageLayout>
         );
     } else {
